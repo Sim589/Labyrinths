b83a103075e93fc2dc2d06e6f5e9574813da4840
Villaq-C <villaquiranchris@gmail.com>
2024-11-08 23:00:26 +0000

    typo fixed

--- frontend-labyrinths/src/pages/home/index.tsx
+++ frontend-labyrinths/src/pages/home/index.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
       <h1>Landing Page</h1>
       <div className="card">
         <p>
-          This is the basic home page from <code>src/pages/home.tsx</code>.
+          This is the awesome home page from <code>src/pages/home.tsx</code>.
         </p>
       </div>
     </>
