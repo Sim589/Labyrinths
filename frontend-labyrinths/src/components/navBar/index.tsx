36724e583bea34e91e9432371953c889c8ada747
Villaq-C <villaquiranchris@gmail.com>
2024-11-09 15:42:56 +0000

    Added nabar and updated routing

---
+++ frontend-labyrinths/src/components/navBar/index.tsx
@@ -0,0 +1,32 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+
+export default function NavBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Button color="inherit" href="/">
+            Home
+          </Button>
+          <Button color="inherit" href="/about">
+            About
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
--- frontend-labyrinths/src/router/index.tsx
+++ frontend-labyrinths/src/router/index.tsx
@@ -1,17 +1,19 @@
-import { createBrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Root from "../pages/home";
+import NavBar from "../components/navBar";
 import ErrorPage from "../pages/errorPage";
 import TemplatePage from "../pages/viteTemplate";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/template",
-    element: <TemplatePage />,
-    errorElement: <ErrorPage />,
-  },
-]);
+export function AppRouter() {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="*" element={<ErrorPage />} />
+        <Route path="/" element={<Root />} />
+        <Route path="/about" element={<TemplatePage />} />
+        <Route path="/template" element={<TemplatePage />} />
+      </Routes>
+    </Router>
+  );
+}
